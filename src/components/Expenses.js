b85f505bfdf1d9efaebd5307c1edcd10a60dbbe6
Jorge Rodriguez <jorgea.rodriguezm91@gmail.com>
2022-07-10 19:47:49 -0500

    Switching regular javascript sintax by arrow function.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,17 +1,22 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 
-function Expenses (props){
 
-    const expenses = props.expenses;
 
+const Expenses = ({ expenses }) => {
     return (
         <div className="expenses">
-            <ExpenseItem date={expenses[0].date} description={expenses[0].description} amount={expenses[0].price}></ExpenseItem>
-            <ExpenseItem date={expenses[1].date} description={expenses[1].description} amount={expenses[1].price}></ExpenseItem>
-            <ExpenseItem date={expenses[2].date} description={expenses[2].description} amount={expenses[2].price}></ExpenseItem>
+        {expenses.map((expense, i) => (
+            <ExpenseItem
+            key={i}
+            description={expense.description}
+            amount={expense.price}
+            date={expense.date}
+            />
+        ))}
         </div>
     );
-}
+};
+
 
 export default Expenses;
