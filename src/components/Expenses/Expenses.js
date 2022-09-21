bc46a21bd53e0dc6e9c71e8010ce148586d79fe7
Jorge Rodriguez <jorgea.rodriguezm91@gmail.com>
2022-09-20 23:35:55 -0500

    Adding function on parent to be called by ExpenseFilter component.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,33 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-
-
+import ExpensesFilter from './ExpensesFilter';
+import React,{useState} from 'react';
 
 const Expenses = ({ expenses }) => {
-    
+
+    const [yearFilter,setYearFilter] = useState('2020'); 
+
+    const onChangeSelectHandler = (year) => {
+        console.log("On Expenses");
+        setYearFilter(year);
+        console.log(yearFilter);
+    };
+
     return (
-        <Card className="expenses">
-        {expenses.map((expense, i) => (
-            <ExpenseItem
-            key={i}
-            description ={expense.description}
-            amount={expense.price}
-            date={expense.date}
-            />
-        ))}
-        </Card>
+        <div>
+            <Card className="expenses">
+            <ExpensesFilter selectedYear={yearFilter} onChangeSelect={onChangeSelectHandler}/>
+            {expenses.map((expense, i) => (
+                <ExpenseItem
+                key={i}
+                description ={expense.description}
+                amount={expense.price}
+                date={expense.date}
+                />
+            ))}
+            </Card>
+        </div>
     );
 };
 
