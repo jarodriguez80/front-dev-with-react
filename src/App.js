63107e717336c1fb8277bcccc45f3b6dab02093a
Jorge Rodriguez <jorgea.rodriguezm91@gmail.com>
2022-09-26 19:33:41 -0500

    Switching stated expenses to consider previous state.

--- src/App.js
+++ src/App.js
@@ -4,37 +4,37 @@ import Card from './components/UI/Card';
 import NewExpense from './components/NewExpenses/NewExpense'
 import React,{useState} from 'react';
 
-const App = ()=> {
+const mockedExpenses=[
+  {
+    id:"a",
+    date: new Date(2022,7,9),
+    description: "Weight machine",
+    price: 160.86
+  },
+  {
+    id:"b",
+    date: new Date(2022,7,9),
+    description: "Smart watch",
+    price:789.80
+  },
+  {
+    id:"c",
+    date: new Date(2022,7,9),
+    description: "Sport shoes UA",
+    price: 143.80
+  }
+];
 
-  const expenses=[
-    {
-      date: new Date(2022,7,9),
-      description: "Weight machine",
-      price: 160.86
-    },
-    {
-      date: new Date(2022,7,9),
-      description: "Smart watch",
-      price:789.80
-    },
-    {
-      date: new Date(2022,7,9),
-      description: "Sport shoes UA",
-      price: 143.80
-    }
-  ];
-  const  [statedExpenses,setStatedExpenses] = useState(expenses);
+const App = ()=> {
+  const  [statedExpenses,setStatedExpenses] = useState(mockedExpenses);
 
   const onAddExpenseHandler = (newExpense) => {
-    console.log("In App.js");    
-    const updatedExpenses = [
-      ...statedExpenses,
-      newExpense
-    ];
-    setStatedExpenses(updatedExpenses);
-    console.log(updatedExpenses);
-      
-    
+    setStatedExpenses((prevExpenses)=>{
+      return [
+        newExpense,
+        ...prevExpenses
+      ];
+    });
   };
   
   return (
